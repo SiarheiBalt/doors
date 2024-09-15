b235622faf1e8361b0e026034f841265536278e2
SiarheiBalt <jh-11@yandex.by>
2024-09-15 15:07:42 +0300

    feat: add logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
 import React, {FC} from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import icon from '../../../public/pr-logo.png';
 
 type Props = {}
 
 const Header: FC<Props> = () => {
     return (
         <header className="bg-slate-50 border-b-2 pt-5 pb-5">
-            <div className="max-w-5xl container m-auto">
-                <Link href="/" className="mr-4">Home</Link>
-                <Link href="./main" className="mr-4">Main</Link>
+            <div className="max-w-5xl container m-auto flex justify-start items-center">
+                <Link href="/" className="mr-4">
+                    <Image
+                        src={icon}
+                        width={122}
+                        height={300}
+                        alt="Picture of the author" />
+                </Link>
+                <Link href="./catalog" className="mr-4 ml-16">КАТАЛОГ</Link>
+                <Link href="./location" className="mr-4">ГДЕ КУПИТЬ</Link>
             </div>
         </header>
     );
